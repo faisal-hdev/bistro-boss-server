1857fe768ee55ddb629b10eb34dbe14261615b74
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-06-23 07:58:02 +0600

    Carts collection database careated and create get,delete,post api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // DB connection here
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s3zryfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,11 +28,39 @@ async function run() {
 
     // Get the database and collection on which to run the operation
     const menuCollection = client.db("bistroDB").collection("menu");
+    const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
-    app.get('/menu', async(req, res)=>{
+    app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart collection
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -53,3 +81,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro boss is sitting on port ${port}`);
 });
+
+/**
+ * ------------------------------------------------------
+ *                  NAMING CONVENTION
+ * ----------------------------------------------------
+ * app.get("/users")
+ * app.get("/users/:id")
+ * app.post("/users")
+ * app.put("users/:id")
+ * app.patch("users/:id")
+ * app.delete("users/:id")
+ */
