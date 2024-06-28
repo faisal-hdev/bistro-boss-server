ab463462b9a311cad0dfac47ba1f3f3bc22f8869
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-06-28 18:19:53 +0600

    Create users delete or update api also users get api

--- index.js
+++ index.js
@@ -38,12 +38,36 @@ async function run() {
       // insert email if user doesn't exist ,
       // you can do this many way (1. email unique, 2. upsert, 3. simple checking)
       const query = { email: user.email };
-
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
         return res.send({ message: "user already exist", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result);
     });
 
